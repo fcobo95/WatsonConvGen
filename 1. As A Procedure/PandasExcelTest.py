669ebd9800a2d659144263ddb84b72158b30f96f
Krakenaitor <fcobo95@gmail.com>
2017-10-24 15:03:06 -0600

    Finished intents in their wholeness. Supervisor approved. Added TODO list for next iteration.

--- 1. As A Procedure/PandasExcelTest.py
+++ 1. As A Procedure/PandasExcelTest.py
@@ -3,10 +3,10 @@ import xlrd
 import numpy as pd
 import matplotlib
 
-theWorkbook = Reader.ExcelFile('WatsonConvGen Excel Template.xlsx')
+theWorkbook = Reader.ExcelFile('../Data Files/WatsonConvGen Excel Template.xlsx')
 theSettings = Reader.read_excel(theWorkbook, sheetname=None)
 theValues = theSettings['Settings']
-theData = theValues.loc[:,"WorkspaceName"]
+theData = theValues.loc[:, "WorkspaceName"]
 theData2 = theData.values
 for _ in theData2:
     print(_)
